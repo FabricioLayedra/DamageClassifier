77409f12b10d4eb24f25329c9bbd7a03b6401025
FabricioLayedra <flayedra@espol.edu.ec>
2018-10-12 16:01:24 -0500

    Updating dataset script

---
+++ scripts/converter.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import pandas as pd
+
+def read_dataset():
+    col_dtypes = {'BaseCliente': 'uint16','BaseEvento': 'uint16','CantidadClientes': 'uint8','CantonCliente': 'category','CantonEvento': 'category',
+        'CiudadCliente': 'category',
+        'CiudadEvento': 'category',
+        'Hora': 'uint8',
+        'ID': 'uint32',
+        'LatitudCliente': 'float64',
+        'LatitudEvento': 'float64',
+        'LongitudCliente': 'float64',
+        'LongitudEvento': 'float64',
+        'NombreSitioEvento': 'category',
+        'ParroquiaCliente': 'category',
+        'ParroquiaEvento': 'category',
+        'ProvinciaCliente': 'category',
+        'ProvinciaEvento': 'category'}
+
+    df_eventos = pd.read_csv("../EventosManabi.csv", encoding = "ISO-8859-1", dtype=col_dtypes, parse_dates=['FechaEvento'], infer_datetime_format=True)
+    return df_eventos
+
+df = read_dataset()
+
+df["BaseCliente"] = df["BaseCliente"].apply(lambda x: int(str(x)[-3:]))
+
+df["BaseEvento"] = df["BaseEvento"].apply(lambda x: int(str(x)[-3:]))
+
+df = df.iloc[:,1:]
+
+df.to_csv("eventos_dataset.csv",index=False)
